d1640bd2d3abc5aff0421820c889b2e1874a6086
Josh Milthorpe <josh.milthorpe@gmail.com>
2023-11-17 03:33:18 -0500

    HIPIFY for Frontier

--- cuda_aware/ping_pong_cuda_aware.cu
+++ cuda_aware/ping_pong_cuda_aware.cu
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <mpi.h>
+#include <cuda.h>
 
 // Macro for checking errors in CUDA API calls
 #define cudaErrorCheck(call)                                                              \
